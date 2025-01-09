e01e09e0a8ad8bcd6d9bad55e3269b1f7ad568c8
Towkai <ji32k7Email@gmail.com>
2025-01-09 16:01:26 +0800

    [Modify]新增0階乘的判斷

--- C/C_P77_FunctionPractice/factorial.c
+++ C/C_P77_FunctionPractice/factorial.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
+/*
+延伸：輸入一個自然數計算皆乘
+*/
+
 int factorial(int x);
 
 void main() {
     int input;
     printf("輸入一個自然數: ");
     scanf("%d", &input);
-    if (input < 0)
+    if (input >= 0)
+        printf("%d! = %d\n", input, factorial(input));
+    else
         printf("輸入錯誤");
-    printf("%d! = %d\n", input, factorial(input));
 }
 
 int factorial(int x)
 {
+    if (x == 0)
+        return 1;
     for (int i = x; i > 0; i--)
         x *= i;
     return x;
