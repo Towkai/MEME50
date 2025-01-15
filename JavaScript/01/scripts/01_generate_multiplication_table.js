d0a40db9e7814c6cb292e9c7a3949f888fb879f9
Towkai <ji32k7Email@gmail.com>
2025-01-15 16:02:08 +0800

    [Modify]將九九乘法表的標題改為黑色、固定九九乘法表的欄寬

--- JavaScript/01/scripts/01_generate_multiplication_table.js
+++ JavaScript/01/scripts/01_generate_multiplication_table.js
@@ -7,10 +7,11 @@ function generate_table()
     let table = document.createElement('table');
     // table.setAttribute("border", "1");
     table.setAttribute("bgcolor", "Aqua");
-    table.setAttribute("style", "color:red;border-style:ridge;border: 5px solid DodgerBlue;border-collapse: collapse")
+    table.setAttribute("style", "color:red;border-style:ridge;border: 5px solid DodgerBlue;border-collapse: collapse;text-align:left;width:max-content")
     let title = document.createElement('caption');
-    let tbody = document.createElement('tbody');
+    title.setAttribute("style", "color:black");
     title.textContent = "九九乘法表";
+    let tbody = document.createElement('tbody');
     table.appendChild(title);
     table.appendChild(tbody);
     
@@ -21,7 +22,9 @@ function generate_table()
         for (let j = 1; j <= 9; j++)
         {
             tr = document.createElement('tr');
-            tr.textContent = `${i} * ${j} = ${i * j}`;
+            td = document.createElement('td');
+            td.textContent = `${i} * ${j} = ${i * j}`;
+            tr.appendChild(td);
             th.appendChild(tr);
         }
         tbody.appendChild(th);
