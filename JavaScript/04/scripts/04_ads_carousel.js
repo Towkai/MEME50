c966ada0c0b945f228b7b853972c6309c1c982d5
Towkai <ji32k7Email@gmail.com>
2025-01-17 20:36:30 +0800

    [Modify]Javascript作業第4題，新增選擇第幾張投影片的功能

--- JavaScript/04/scripts/04_ads_carousel.js
+++ JavaScript/04/scripts/04_ads_carousel.js
@@ -1,19 +1,30 @@
 window.addEventListener("load", (event) => {
-  generate_carousel();
+    generate_carousel();
     onclick_startpause(null);
   });
 
 var img_urls = [
-  { url: "images/padoru-0.png", link:"https://youtu.be/dQ_d_VKrFgM"},
-  { url: "images/padoru-1.png", link:"https://youtu.be/ZtOykzpYJXk?t=1m20s"},
-  { url: "images/padoru-2.png", link:"https://youtu.be/KoyGsLyUjJE?t=3m45s"},
-  { url: "images/padoru-3.png", link:"https://youtu.be/dQw4w9WgXcQ"},
+  { url: "https://raw.githubusercontent.com/Towkai/MEME50/refs/heads/main/JavaScript/04/images/padoru-0.png", link:"https://youtu.be/dQ_d_VKrFgM"},
+  { url: "https://raw.githubusercontent.com/Towkai/MEME50/refs/heads/main/JavaScript/04/images/padoru-1.png", link:"https://youtu.be/ZtOykzpYJXk?t=1m20s"},
+  { url: "https://raw.githubusercontent.com/Towkai/MEME50/refs/heads/main/JavaScript/04/images/padoru-2.png", link:"https://youtu.be/KoyGsLyUjJE?t=3m45s"},
+  { url: "https://raw.githubusercontent.com/Towkai/MEME50/refs/heads/main/JavaScript/04/images/padoru-3.png", link:"https://youtu.be/dQw4w9WgXcQ"},
 ];
 
 var page = 0;
 var clock = null;
 var speed_default = 200; //毫秒
 var speed = 200;
+var button_width = "50px"
+
+function char_unselect(n)
+{
+  return String.fromCharCode(0x2460 + n);
+}
+function char_selected(n)
+{
+  // return String.fromCharCode(0x2776 + n);
+  return String.fromCharCode(0x24F5 + n);
+}
 
 function generate_carousel()
 {
@@ -24,13 +35,14 @@ function generate_carousel()
   caption.textContent = "ads carousel";
   table.appendChild(caption);
 
-  create_imgs(table);
-  create_controlbar(table);
+  table.appendChild(create_imgs());
+  table.appendChild(create_progressbar());
+  table.appendChild(create_controlbar());
   
   document.body.appendChild(table);
 }
 
-function create_imgs(table)
+function create_imgs()
 {
   let tr = document.createElement("tr");
   let td = document.createElement('td');
@@ -45,7 +57,7 @@ function create_imgs(table)
     td.appendChild(a);
   }
   tr.appendChild(td);
-  table.appendChild(tr);
+  return tr;
 }
 
 function control_carousel(e)
@@ -53,24 +65,48 @@ function control_carousel(e)
   page = (page + e) % img_urls.length;
   if (page < 0)
     page = img_urls.length - 1;
+  set_carousel(page);
+}
+function set_carousel(e)
+{
+  page = e;
   for (let i = 0; i < img_urls.length; i++)
   {
     let img = document.getElementById("carousel" + i);
     img.setAttribute("style", `display: ${i == page ? '' : 'none'}`);
+    let button = document.getElementById("progress" + i);
+    button.textContent = i == page ? char_selected(i) : char_unselect(i);
   }
 }
 
-function create_controlbar(table)
+function create_progressbar()
 {
   let tr = document.createElement("tr");
-  tr.setAttribute("style", "text-align:center; display: flex; justify-content: space-around;");
+  tr.setAttribute("style", "text-align:center; display: flex; justify-content: space-around;margin:0px 25px 0px 25px;");
+  for (let i = 0; i < img_urls.length; i++)
+  {
+    let button = document.createElement("button");
+    button.setAttribute("style", `width: ${button_width}`)
+    button.setAttribute("id", "progress" + i);
+    button.addEventListener("click", (e) => {pause_carousel(null);set_carousel(i)});
+    button.value = i;
+    button.textContent = char_unselect(i + 1);
+    tr.appendChild(button);
+  }
+  return tr;
+}
+
+function create_controlbar()
+{
+  let tr = document.createElement("tr");
+  tr.setAttribute("style", "text-align:center; display: flex; justify-content: space-around;margin:0px 25px 0px 25px;");
   let last = document.createElement('button');
   let startpause = document.createElement('button');
   let next = document.createElement('button');
   startpause.setAttribute("id", "button_startpause");
-  last.setAttribute("style", "width: 50px");
-  startpause.setAttribute("style", "width: 50px");
-  next.setAttribute("style", "width: 50px");
+  last.setAttribute("style", `width: ${button_width}`);
+  startpause.setAttribute("style", `width: ${button_width}`);
+  next.setAttribute("style", `width: ${button_width}`);
   last.textContent = '⮪';
   startpause.textContent = '▶';
   next.textContent = '⮫';
@@ -83,8 +119,7 @@ function create_controlbar(table)
 
   let speed = create_speedbar();
   tr.appendChild(speed);
-
-  table.appendChild(tr);
+  return tr;
 }
 
 function create_speedbar()
@@ -94,7 +129,13 @@ function create_speedbar()
   select.setAttribute("id", "speed");
   select.setAttribute("name", "speed");
   select.setAttribute("aria-label", "speed");
-  let options = create_opions([0.25, 0.5, 1.0, 1.25, 1.5, 1.75, 2.0, 3.0, 4.0, 5.0, 6.0]);
+  select.setAttribute("style", `width: ${button_width}`);
+  let speed_list = [];
+  for (let i = 0.5; i <= 6; i += 0.5)
+  {
+    speed_list.push(i);
+  }
+  let options = create_opions(speed_list);
   for (let option of options)
     select.appendChild(option);
   return select;
@@ -118,7 +159,7 @@ function create_opions(values)
       option.setAttribute("value", values[i]);
       if (values[i] == 1)
         option.setAttribute("selected","");
-      option.textContent = 'x' + values[i].toFixed(2);
+      option.textContent = 'x' + values[i].toFixed(1);
       options.push(option);
     }
     return options;
