52b7ba1169ad3bbdb16a94e3c8cf6ea6ae8b9a64
Towkai <ji32k7Email@gmail.com>
2025-03-03 16:37:51 +0800

    [Modify]Nodejs作業，搜尋功能完成

--- Nodejs/homework/controller_search.js
+++ Nodejs/homework/controller_search.js
@@ -3,20 +3,19 @@ const csv_file = "employee.csv";
 const ejs_file = "insert.ejs";
 const csv2json = require('convert-csv-to-json');
 var json = csv2json.fieldDelimiter(',').getJsonFromCsv(csv_file);
-
-
+const employ = require('./employ.js').employ;
 const multilang = require('./multilang.js');
 
 function search_csv(body) {
+    console.log(body);
+
     let keys = Object.keys(body);
     let result = [];
     for (let i = 0; i < json.length; i++)
         for (let j = 0; j < keys.length; j++)
         {
-            let term = true;
             if (body[keys[j]] == '')
                 continue;
-
             if (json[i][keys[j]] == (body[keys[j]]))
                 if (json[i][keys[j]].toLowerCase().includes(body[keys[j]]))
                     result.push(json[i]);
@@ -29,7 +28,8 @@ exports.view_search = (req, res) => {
     let text = multilang.getCurrentLang(req.headers['accept-language']);
     let selected = req.query;
     res.render('search.ejs', {  //固定用法：建立名為"views"的資料夾，將"insert.ejs"放在裡面
-        input_type: multilang.employ.type,
+        data_type: employ.type,
+        input_value: req.query,
         caption: text.caption.search,
         options: text.content,
         selected: selected,
@@ -40,10 +40,19 @@ exports.feature_search = (req, res) => {
     let text = multilang.getCurrentLang(req.headers['accept-language']);
 
     res.render('search.ejs', {  //固定用法：建立名為"views"的資料夾，將"insert.ejs"放在裡面
-        input_type: multilang.employ.type,
+        data_type: employ.type,
+        input_value: req.body,
         caption: text.caption.search,
         options: text.content,
-        selected: '',
+        // selected: '',
         result: search_csv(req.body)
+        // result: {}
     });
 };
+
+function get_deptnos() {
+    const deptnos = new Set();
+    for (let i = 0; i < json.length; i++)
+        deptnos.add(json[i]["deptno"]);
+    return deptnos;
+}
