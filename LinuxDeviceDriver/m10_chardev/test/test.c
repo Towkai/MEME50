c0516120f3e6b547417296f7d61a4d04e31e5feb
Towkai <ji32k7Email@gmail.com>
2025-03-22 11:16:58 +0800

    [Modify]驅動程式第四天課程

---
+++ LinuxDeviceDriver/m10_chardev/test/test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>	//open
+#include<unistd.h>	//exit
+#include<sys/ioctl.h>	//ioctl
+
+static char *dev="/dev/hello",message[100];
+
+int main()
+{
+  int file_desc;
+
+  // open the device file, ex: /dev/hello
+
+  // read from the device file
+
+  printf("get_msg message:%s\n",message);
+
+  // write to the device file
+
+  // close file
+
+  return 0;
+}
